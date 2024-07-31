d3d9b545b646bd2d65fc7e5ad630d478410db9c1
awinooliyo <erick.otieno89@gmail.com>
2024-07-31 20:18:08 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,34 @@ filtered_logger.py
 """
 
 import re
+import logging
+from typing import List
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format(
+        '|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields, redaction, message, separator):
-    pattern = '|'.join([f'(?<={field}=)[^{separator}]*' for field in fields])
-    return re.sub(pattern, redaction, message)
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """Returns the log message with certain fields obfuscated.
+
+    Args:
+        fields (List[str]): a list of strings representing all fields to
+        obfuscate.
+        redaction (str): a string representing by what the field will be
+        obfuscated.
+        message (str): a string representing the log line.
+        separator (str): a string representing by which character is
+        separating all fields in the log line (message).
+
+    Returns:
+        str: the log message obfuscated.
+    """
+    extract, replace = patterns["extract"], patterns["replace"]
+    return re.sub(extract(fields, separator), replace(redaction), message)
