f738f3ba6141f324d0d41ecd67aa259e16eaaa3a
awinooliyo <erick.otieno89@gmail.com>
2024-10-07 13:02:23 +0300

    task 1

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" User Module """
+
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        return (f"<User(id={self.id}, email='{self.email}', "
+                f"session_id='{self.session_id}', "
+                f"reset_token='{self.reset_token}')>")
+
+
+engine = create_engine('sqlite:///example.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+new_user = User(email='example@example.com',
+                hashed_password='hashed_password_example')
+session.add(new_user)
+session.commit()
