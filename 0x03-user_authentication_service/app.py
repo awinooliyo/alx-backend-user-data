3772789b3031e10e288576c6b5f28527036fa54a
awinooliyo <erick.otieno89@gmail.com>
2024-10-07 17:24:24 +0300

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,12 +9,10 @@ app = Flask(__name__)
 
 AUTH = Auth()
 
-
 @app.route("/", methods=["GET"])
 def home():
     """Return a welcome message as a JSON payload."""
     return jsonify({"message": "Bienvenue"})
-
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
@@ -38,3 +36,9 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+    except NoResultFound:
+        return jsonify({"message": "user not found"}), 404  # Optional if relevant to your logic
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
